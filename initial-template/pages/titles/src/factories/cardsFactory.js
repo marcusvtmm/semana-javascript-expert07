80173575438ddd970aec410df6f66f75f92dfc14
MarcusV <mvtmm2018@gmail.com>
2023-03-05 13:12:27 -0300

    Class 01 part 01: First Web Worker

--- initial-template/pages/titles/src/factories/cardsFactory.js
+++ initial-template/pages/titles/src/factories/cardsFactory.js
@@ -1,12 +1,21 @@
 import CardsController from "./../controllers/cardsController.js"
 import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
+
+const cardListWorker = new Worker( './src/workers/cardÇistWorker',{type:'module'})
+
+
+cardListWorker.postMessage('Hello World')
+
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
     return CardsController.initialize({
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
+      service: new CardsService({
+        dbUrl: `${rootPath}/assets/database.json`,
+        cardListWorker
+      })
     })
   }
 }
